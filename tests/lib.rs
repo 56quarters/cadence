0212a9a168c731463b38e768cacf6febf24e1b24
Nick Pillitteri <nick@tshlabs.org>
2015-11-25 19:40:19 -0500

    Add performance integration tests (can't get them to run yet)

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,9 +2,14 @@
 //!
 //!
 
+#![feature(test)]
+
+extern crate test;
 extern crate statsd;
 
 use std::net::{UdpSocket};
+
+use test::Bencher;
 
 use statsd::client::{
     DEFAULT_PORT,
@@ -48,9 +53,14 @@ fn test_statsd_client_as_counter() {
     let client = new_local_client("counter.test");
     let holder = CounterHolder{counter: &client};
 
-    for i in 0..45 {
-        holder.counter.count("some.counter.metric", i, None);
-    }
+    holder.counter.count("some.counter.metric", 13, None).unwrap();
+}
+
+
+#[bench]
+fn test_statsd_client_counter_performance(b: &mut Bencher) {
+    let client = new_local_client("counter.perf");
+    b.iter(|| client.count("some.counter.metric", 26, None).unwrap())
 }
 
 
@@ -59,9 +69,14 @@ fn test_statsd_client_as_timer() {
     let client = new_local_client("timer.test");
     let holder = TimerHolder{timer: &client};
 
-    for i in 10..35 {
-        holder.timer.time("some.timer.metric", i, None);
-    }
+    holder.timer.time("some.timer.metric", 25, None).unwrap();
+}
+
+
+#[bench]
+fn test_statsd_client_timer_performance(b: &mut Bencher) {
+    let client = new_local_client("timer.perf");
+    b.iter(|| client.time("some.timer.metric", 50, None).unwrap())
 }
 
 
@@ -70,7 +85,12 @@ fn test_statsd_client_as_gauge() {
     let client = new_local_client("gauge.test");
     let holder = GaugeHolder{gauge: &client};
 
-    for i in 90..100 {
-        holder.gauge.gauge("some.gauge.metric", i);
-    }
+    holder.gauge.gauge("some.gauge.metric", 98).unwrap();
+}
+
+
+#[bench]
+fn test_statsd_client_gauge_performance(b: &mut Bencher) {
+    let client = new_local_client("gauge.perf");
+    b.iter(|| client.gauge("some.gauge.metric", 98).unwrap())
 }
