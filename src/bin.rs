2b66426ff03406a1cb5db6233c62948a413b52ae
Nick Pillitteri <nick@tshlabs.org>
2015-11-26 22:50:10 -0500

    Unwrap results

--- src/bin.rs
+++ src/bin.rs
@@ -11,9 +11,10 @@ fn main() {
     let metric_host = ("127.0.0.1", DEFAULT_PORT);
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
     let sink = UdpMetricSink::new(metric_host, socket);
+    //let sink = ConsoleMetricSink;
     let client = StatsdClient::new("foo.prefix", sink);
-    client.count("some.key", 4, None);
-    client.gauge("some.key", 42);
-    client.time("some.key", 15, None);
+    client.count("some.key", 4, None).unwrap();
+    client.gauge("some.key", 42).unwrap();
+    client.time("some.key", 15, None).unwrap();
 }
 
