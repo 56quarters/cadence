7145d1558746f3dabacbb11d5d007617977ee06d
Nick Pillitteri <nick@tshlabs.org>
2015-11-30 17:26:27 -0500

    Use reference for sending actual metric

--- src/client/metrics.rs
+++ src/client/metrics.rs
@@ -59,7 +59,7 @@ impl<T: MetricSink> StatsdClient<T> {
         StatsdClient{prefix: trimmed.to_string(), sink: Box::new(sink)}
     }
     
-    fn send_metric<M: ToMetricString>(&self, metric: M) -> MetricResult<()> {
+    fn send_metric<M: ToMetricString>(&self, metric: &M) -> MetricResult<()> {
         let metric_string = metric.to_metric_string();
         let written = try!(self.sink.send(&metric_string));
         debug!("Wrote {} ({} bytes)", metric_string, written);
@@ -71,7 +71,7 @@ impl<T: MetricSink> StatsdClient<T> {
 impl<T: MetricSink> Counted for StatsdClient<T> {
     fn count(&self, key: &str, count: u64, sampling: Option<f32>) -> MetricResult<()> {
         let counter = Counter::new(make_key(&self.prefix, key), count, sampling);
-        self.send_metric(counter)
+        self.send_metric(&counter)
     }
 }
 
@@ -79,7 +79,7 @@ impl<T: MetricSink> Counted for StatsdClient<T> {
 impl<T: MetricSink> Timed for StatsdClient<T> {
     fn time(&self, key: &str, time: u64, sampling: Option<f32>) -> MetricResult<()> {
         let timer = Timer::new(make_key(&self.prefix, key), time, sampling);
-        self.send_metric(timer)
+        self.send_metric(&timer)
     }
 }
 
@@ -87,7 +87,7 @@ impl<T: MetricSink> Timed for StatsdClient<T> {
 impl<T: MetricSink> Gauged for StatsdClient<T> {
     fn gauge(&self, key: &str, value: i64) -> MetricResult<()> {
         let gauge = Gauge::new(make_key(&self.prefix, key), value);
-        self.send_metric(gauge)
+        self.send_metric(&gauge)
     }
 }
 
