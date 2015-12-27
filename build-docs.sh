c40c2bcd6aa73ffb2dba091a4643c27714f9e598
Nick Pillitteri <nick@tshlabs.org>
2015-12-27 01:58:22 -0500

    Rewrite doc build script

--- build-docs.sh
+++ build-docs.sh
@@ -1,61 +1,36 @@
 #!/bin/bash -x
-#
-# Portions of this doc build script, copyright (c) 2013 by Armin Ronacher.
-#
-# Some rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above
-#   copyright notice, this list of conditions and the following
-#   disclaimer in the documentation and/or other materials provided
-#   with the distribution.
-#
-# * The names of the contributors may not be used to endorse or
-#   promote products derived from this software without specific
-#   prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
 
+# Kill the existing target directory and built docs
 cargo clean
 cargo doc --no-deps
+rev=`git rev-parse --short HEAD`
 
+# Create a new target directory with an empty git repo
+prev=`pwd`
 mkdir target/uploads
 cd target/uploads
 git init
 
+# Add generated docs and a redirect page to our new repo
 cp -R ../doc/* .
 cat <<EOF > index.html
 <!doctype html>
 <html>
 <head>
-<title>cadence</title>
-<meta http-equiv="refresh" content="0; ./cadence/" />
+<title></title>
+<meta http-equiv="refresh" content="0; url=./cadence/" />
 </head>
 </html>
 EOF
 
+# Add the real github repo as a remote and force-push everything
+# we just committed to our new repo locally to the gh-pages branch
 git add --all .
-git commit -m "Built docs"
+git commit -m "Building Cadence docs at ${rev}"
 git checkout -b gh-pages
-git remote add origin git@github.com:tshlabs/cadence.git
-git push --force origin gh-pages
+git remote add github git@github.com:tshlabs/cadence.git
+git push --force github gh-pages
 
-cd ../..
+# Kill the target directory and hence the local repo
+cd $prev
 cargo clean
