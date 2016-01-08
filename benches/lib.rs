966154770e45fe7536c0b65816f57a6661b7e438
Nick Pillitteri <nick@tshlabs.org>
2016-01-08 01:29:44 -0500

    Benchmarks for all public methods

--- benches/lib.rs
+++ benches/lib.rs
@@ -14,30 +14,55 @@ use cadence::{
 };
 
 
+fn new_nop_client() -> StatsdClient<NopMetricSink> {
+    StatsdClient::from_sink("test.bench", NopMetricSink)
+}
+
+
 #[bench]
-fn test_benchmark_statsdclient_counter(b: &mut Bencher) {
-    let client = StatsdClient::from_sink("test.bench", NopMetricSink);
+fn test_benchmark_statsdclient_count(b: &mut Bencher) {
+    let client = new_nop_client();
     b.iter(|| client.count("some.counter", 4));
 }
 
 
 #[bench]
-fn test_benchmark_statsdclient_timer(b: &mut Bencher) {
-    let client = StatsdClient::from_sink("test.bench", NopMetricSink);
+fn test_benchmark_statsdclient_incr(b: &mut Bencher) {
+    let client = new_nop_client();
+    b.iter(|| client.incr("some.counter.incr"));
+}
+
+
+#[bench]
+fn test_benchmark_statsdclient_decr(b: &mut Bencher) {
+    let client = new_nop_client();
+    b.iter(|| client.decr("some.counter.decr"));
+}
+
+
+#[bench]
+fn test_benchmark_statsdclient_time(b: &mut Bencher) {
+    let client = new_nop_client();
     b.iter(|| client.time("some.timer", 4));
 }
 
 
 #[bench]
 fn test_benchmark_statsdclient_gauge(b: &mut Bencher) {
-    let client = StatsdClient::from_sink("test.bench", NopMetricSink);
+    let client = new_nop_client();
     b.iter(|| client.gauge("some.gauge", 4));
 }
 
 
 #[bench]
 fn test_benchmark_statsdclient_meter(b: &mut Bencher) {
-    let client = StatsdClient::from_sink("test.bench", NopMetricSink);
+    let client = new_nop_client();
     b.iter(|| client.meter("some.meter", 4));
 }
 
+
+#[bench]
+fn test_benchmark_statsdclient_mark(b: &mut Bencher) {
+    let client = new_nop_client();
+    b.iter(|| client.mark("some.meter.mark"));
+}
