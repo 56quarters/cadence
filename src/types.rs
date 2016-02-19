1232cf6619b8a9921f2df957112dfb92deb2333a
Nick Pillitteri <nick@tshlabs.org>
2016-02-18 23:45:19 -0500

    Update copyright for 2016

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 // Cadence - An extensible Statsd client for Rust!
 //
-// Copyright 2015 TSH Labs
+// Copyright 2015-2016 TSH Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
