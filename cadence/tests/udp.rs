9aaed19e0fcc3ce03a30afd8623828f6120a0184
Nick Pillitteri <nick@56quarters.xyz>
2026-04-11 10:09:24 -0400

    Use resolving behavior in integration tests
    This changes the UDP sinks in integration test to use the re-resolving
    behavior. Also add an example of how to configure sinks to use this
    in the examples directory.
    
    Related #236 #222

--- cadence/tests/udp.rs
+++ cadence/tests/udp.rs
@@ -1,5 +1,6 @@
 use cadence::{BufferedUdpMetricSink, QueuingMetricSink, StatsdClient, UdpMetricSink, DEFAULT_PORT};
 use std::net::UdpSocket;
+use std::time::Duration;
 use utils::run_arc_threaded_test;
 
 mod utils;
@@ -8,7 +9,11 @@ const TARGET_HOST: (&str, u16) = ("127.0.0.1", DEFAULT_PORT);
 
 fn new_udp_client(prefix: &str) -> StatsdClient {
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
-    let sink = UdpMetricSink::from(TARGET_HOST, socket).unwrap();
+    let sink = UdpMetricSink::builder()
+        .with_resolver_period(Duration::from_millis(100))
+        .with_resolver_error_handler(|e| panic!("resolve error: {}", e))
+        .build(TARGET_HOST, socket)
+        .unwrap();
     StatsdClient::from_sink(prefix, sink)
 }
 
@@ -28,17 +33,17 @@ fn new_queuing_buffered_udp_client(prefix: &str) -> StatsdClient {
 #[test]
 fn test_statsd_client_udp_sink_single_threaded() {
     let client = new_udp_client("cadence");
-    run_arc_threaded_test(client, 1, 1);
+    run_arc_threaded_test(client, 1, 10);
 }
 
 #[test]
 fn test_statsd_client_buffered_udp_sink_single_threaded() {
     let client = new_buffered_udp_client("cadence");
-    run_arc_threaded_test(client, 1, 1);
+    run_arc_threaded_test(client, 1, 10);
 }
 
 #[test]
 fn test_statsd_client_queuing_buffered_udp_sink_single_threaded() {
     let client = new_queuing_buffered_udp_client("cadence");
-    run_arc_threaded_test(client, 1, 1);
+    run_arc_threaded_test(client, 1, 10);
 }
